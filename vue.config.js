74c7748d45e6d6ad6d0f21d80746b70fcfc2a03b
newplum <1105372736@qq.com>
2022-02-12 13:59:33 +0800

    second

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,10 @@
  * @Date: 2021-04-28 18:21:34
  * @FilePath: \my-site\vue.config.js
  * @LastEditors: lilingyun
- * @LastEditTime: 2022-02-12 13:49:08
+ * @LastEditTime: 2022-02-12 13:59:27
  */
 module.exports = {
-  publicPath: '/site/',
+  // publicPath: '/site/',
   // devServer: {
   //     proxy: {
   //         "/api": {
