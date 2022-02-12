78280a91bb0792b7f694ff0fe8b8f54f98f8e4db
newplum <1105372736@qq.com>
2022-02-12 14:58:18 +0800

    second

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,12 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: lilingyun
+ * @Date: 2022-02-12 13:24:44
+ * @FilePath: \my-site\src\router\index.js
+ * @LastEditors: lilingyun
+ * @LastEditTime: 2022-02-12 14:58:12
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
@@ -5,8 +14,8 @@ import routes from './routes'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: 'history',
-    routes
-  })
+  mode: 'hash',
+  routes,
+})
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,10 @@
  * @Date: 2021-04-28 18:21:34
  * @FilePath: \my-site\vue.config.js
  * @LastEditors: lilingyun
- * @LastEditTime: 2022-02-12 14:42:50
+ * @LastEditTime: 2022-02-12 14:46:32
  */
 module.exports = {
-  publicPath: '/',
+  publicPath: '/site/',
   // devServer: {
   //     proxy: {
   //         "/api": {
