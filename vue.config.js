8bb6b0949340483aeeb029557cef1b506e74fe80
newplum <1105372736@qq.com>
2022-02-12 13:02:37 +0800

    fix: ci

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: lilingyun
+ * @Date: 2021-04-28 18:21:34
+ * @FilePath: \my-site\vue.config.js
+ * @LastEditors: lilingyun
+ * @LastEditTime: 2022-02-12 13:02:28
+ */
 module.exports = {
-    // devServer: {
-    //     proxy: {
-    //         "/api": {
-    //             target: ""
-    //         }
-    //     }
-    // }
+  base: 'https://newplum.github.io/site',
+  // devServer: {
+  //     proxy: {
+  //         "/api": {
+  //             target: ""
+  //         }
+  //     }
+  // }
 }
